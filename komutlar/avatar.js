9c9d2392dff6b66523a9da6eca17f4d908130892
AYYILDIZ999 <70440392+AYYILDIZ999@users.noreply.github.com>
2021-01-18 18:05:58 +0300

    Add files via upload

---
+++ komutlar/avatar.js
@@ -0,0 +1,23 @@
+﻿module.exports.operate = ({client, msg, args}) => {
+  if (msg.channel.id !== "797883775416926278") return msg.channel.send("<#797883775416926278>");
+  let kullanici = msg.mentions.users.first() || client.users.cache.get(args[0]) || msg.author;
+  let avatar = kullanici.avatarURL({ dynamic: true, size: 2048 });
+  msg.channel.send({
+    embed: {
+      author: { name: kullanici.tag, icon_url: avatar },
+      description: `[Resim Adresi](${avatar})`,
+      footer: {
+        text: `${msg.member.displayName} tarafından istendi.`,
+        icon_url: msg.author.avatarURL({ dynamic: true })
+      },
+      image: { url: avatar },
+      timestamp: new Date(),
+      color: client.favoriRenkler[Math.floor(Math.random() * client.favoriRenkler.length)]
+    }
+  });
+};
+
+module.exports.help = {
+  name: "avatar",
+  alias: ["gif", "pp"]
+};
